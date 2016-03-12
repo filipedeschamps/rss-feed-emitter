ccc789305265988bb36cbb0fbacf56c952ca1233
Matheus Lima <matheusml90@gmail.com>
2016-03-12 15:52:11 -0300

    Adding webpack UMD uglified version for browsers

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var webpack = require('webpack');
+var nodeExternals = require('webpack-node-externals');
+
+var env = process.env.NODE_ENV
+
+var config = {
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
+    ]
+  },
+  output: {
+    library: 'RssFeedEmitter',
+    libraryTarget: 'umd'
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  resolve: {
+    extensions: ['', '.js']
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(env)
+    })
+  ]
+};
+
+if (env === 'production') {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true,
+        warnings: false
+      }
+    })
+  )
+}
+
+module.exports = config
